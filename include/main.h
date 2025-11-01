4ed37db19d381288a46cdae15d67dd0c451b1702
Mateusz Tyc <34238398+noodlekid@users.noreply.github.com>
2025-11-01 17:23:50 -0400

    feat: project setup (#1)
    * chore: add gitignore
    
    * feat: initial project structure
    
    docs: clean up file
    
    * feat: unfuck clocks
    
    * docs: add readme
    
    * feat: inital CI pipeline
    
    * docs: update README.md
    
    Removed unnecessary commands for generating compile_commands.json

---
+++ include/main.h
@@ -0,0 +1,16 @@
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  */
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
